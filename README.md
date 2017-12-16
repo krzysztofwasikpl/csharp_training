fb56360b16e8afa87286685eb7b9f17e0a3d56a5
vector <vector@vectors-MacBook-Air.local>
2017-12-16 22:38:54 +0100

    Modified README.md file and added simple project for lambda usage

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # csharp_training
+
+Platform configuration:
+Mac OS High Sierra
+Visual Studio Community for Mac (7.2.2)
--- lambdas/Program.cs
+++ lambdas/Program.cs
@@ -4,9 +4,18 @@ namespace lambdas
 {
     class MainClass
     {
+        public delegate int MyDelegate(int x);
+
         public static void Main(string[] args)
         {
             Console.WriteLine("project: lambdas");
+
+            MyDelegate foo = (x) => x * x ;
+            Console.WriteLine("foo 1 for x=2 is " + foo(2));
+
+            foo = (x) => 3 * x;
+            Console.WriteLine("foo 2 for x=2 is " + foo(2));
+
         }
     }
 }
